a4b6e11293e7b8449239c4bca3f48c323f972401
soko paul <sopapaso73@gmail.com>
2020-04-14 06:01:51 +0300

    adjust page title and fix server port display

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-app.listen(port, () => console.log(`Gator app listening on port $port !`));
+app.listen(port, () => console.log(`Gator app listening on port ${port} !`));
